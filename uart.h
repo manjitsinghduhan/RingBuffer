0b6b870d06a514016565eb41a2c5a68d98efec78
manjitsinghduhan <duhan.manjit@gmail.com>
2017-03-29 14:00:24 +0200

    Update uart.h

--- uart.h
+++ uart.h
@@ -8,8 +8,19 @@
 
 /*Assigning some random address to avoid the compile error
  * used UART_READ_ADDR as per requirement */
-
 #define UART_READ_ADDR 0x30610000
+
+/******************************************************************************
+* Function Name:  UART_init
+**************************************************************************//**
+*   @brief      init the uart with predefined BAURD rate, parity and STOP bit
+*   @param[in] 	None
+*   @param[out] None
+*   @retval     None
+*   @warning    None
+*   @remark     None
+*//***************************************************************************/
+void UART_init();
 
 
 #endif /* UART_H_ */
