566bf519906bf0faa36b8c3cafd127ddc259b484
manjitsinghduhan <duhan.manjit@gmail.com>
2017-03-29 13:59:06 +0200

    Update uart.C

--- uart.C
+++ uart.C
@@ -5,24 +5,44 @@
 #include "assert.h"
 
 RingBuffer *rxBuf;
-
+/******************************************************************************
+* Function Name:  UART_init
+**************************************************************************//**
+*   @brief      init the uart with predefined BAURD rate, parity and STOP bit
+*   @param[in] 	None
+*   @param[out] None
+*   @retval     None
+*   @warning    None
+*   @remark     None
+*//***************************************************************************/
 void UART_init()
 {
 	/*driver init of that UART e.g. UART 0*/
 
 	/*init the Rx interrupt*/
 
-	/*ring buffer init*/
+	/*ring buffer init & asset to check that buffer is initialized properly*/
 	assert(ringBufInit(rxBuf, RX_BUFF_SIZE));
 }
 
-
+/******************************************************************************
+* Function Name:  UART_RxISR
+**************************************************************************//**
+*   @brief      UART Rx ISR routine which is configured such that the interrupt 
+*		is generated when new data is present in the Register.
+*   @param[in] 	None
+*   @param[out] None
+*   @retval     None
+*   @warning    None
+*   @remark     None
+*//***************************************************************************/
 
 void UART_RxISR(void)
 {
 	/*As we are going to read from a memory register so declaring it volatile*/
 	 uint8_t *data;
 
+	/*Read the data from memory mapped adddress*/
 	data = (volatile unsigned char *) UART_READ_ADDR;
 
 	/*write the char value in the ring buffer*/
@@ -30,4 +50,3 @@ void UART_RxISR(void)
 
 	/*clear the interrupt mask and if required enable interrupt again*/
 }
-
